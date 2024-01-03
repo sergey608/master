fbe68ed952b82faaf371019a4de407115de5dc19
sergey608 <krotiksergeji1284941@gmail.com>
2024-01-03 14:02:37 +0300

    Устронил ошибки

--- C#/Seminars/Sem1/Program.cs
+++ C#/Seminars/Sem1/Program.cs
@@ -2,7 +2,7 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Input second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1/num2 == num2)
+if(num1 == num2 * num2)
 {
     System.Console.WriteLine($"{num1} is quad of {num2}");
 } else
