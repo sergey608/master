a36a4b86248771de4b84cbfee60554b7e6c57a9f
sergey608 <krotiksergeji1284941@gmail.com>
2024-01-12 11:46:47 +0300

    GeekBrains_HomeWork

---
+++ zd2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите координату X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите координату Y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Точка находится в первой четверти.");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Точка находится во второй четверти.");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Точка находится в третьей четверти.");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("Точка находится в четвертой четверти.");
+        }
+    }
+}
